a42f1c3f520460b596a3adbf2ac6f3c2f01db7ad
Harry Gustave <harry23gust@gmail.com>
2021-03-14 01:08:26 -0600

    added new js file for questions, updated HTML and CSS to include question and result box and styles

--- assest/script.js
+++ assest/script.js
@@ -1,19 +1,50 @@
-function startTimer(){
-    var counter = 60;
-    setInterval(function() {
-      counter--;
-      if (counter >= 0) {
-        span = document.getElementById("counter");
-        span.innerHTML = counter;
-      }
-      if (counter === 0) {
-          alert('sorry, out of time');
-          clearInterval(counter);
-      }
-    }, 1000);
-  }
-  function start()
-  {
-      document.getElementById("counter").style="color: blue;";
-      startTimer();
-  };
+const btn = document.getElementById(".quizbtn button");
+
+const quiz_box = document.querySelector(".quiz_box");
+const result_box = document.querySelector(".result_box");
+const option_list = document.querySelector(".option_list");
+const time_line = document.querySelector("header .time_line");
+const timeText = document.querySelector(".timer .time_left_txt");
+const timeCount = document.querySelector(".timer .timer_sec");
+
+
+
+
+ 
+quizbtn.onclick = ()=>{
+  
+  quiz_box.classList.add("activeQuiz"); //show quiz box
+  showQuetions(0); //calling showQestions function
+  queCounter(1); //passing 1 parameter to queCounter
+  startTimer(15); //calling startTimer function
+  startTimerLine(0); //calling startTimerLine function
+}
+
+
+
+  // add variable that will contain all the questions 
+
+
+
+  //  add event listener that will register when user clicks on start button
+
+
+
+
+  // when button is clicked, start time function, and prompt the user with a question that they have to answer
+
+
+
+
+  // confirm if answer is  valid || or not, 
+
+
+  //  if answer is valid, present them another question
+
+
+  // if answered incorectly, subtract time from timer
+
+  // kee track of score 
+
+
+  //whe time is up gave is over, or when all questions are answered  game is over 
---
+++ questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "java script is a ...",
+    answer: "lightweight programming language",
+    options: [
+      "Hyper Text Preprocessor",
+      "The same a java",
+      "lightweight programming language",
+      "an object-oriented programming language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does DOM stand for?",
+    answer: "Document Object Model",
+    options: [
+      
+      "Descendent of Mulan",
+      "Document Object Model",
+      "Digital Object Memory"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  
